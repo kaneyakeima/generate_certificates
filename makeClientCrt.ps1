28c3d2f6e02d7c21468dbd182562892c49199608
kanepon <cindyrotus@gmail.com>
2023-05-15 13:38:35 +0900

    20230515-133835

--- makeClientCrt.ps1
+++ makeClientCrt.ps1
@@ -1,13 +1,12 @@
 # Reads SAN and email address from external text file and issues certificate
-$userList = Import-Csv -Path ".\user-list.csv" # External text file path
+$userList = Import-Csv -Path ".\user-list.csv"
 foreach ($user in $userList) {
-  $san = $user.SAN # SAN column of CSV file
-  $email = $user.Email # Email column of CSV file
   $certProps = @{
-    SubjectName = "CN=$san"
+    SubjectName = "CN=$user.SAN"
     CertStoreLocation = "cert:\LocalMachine\My"
     Template = "Machine"
-    DnsName = $san
+    DnsName = $user.DNS
   }
   Get-Certificate @certProps
 }
+#Get-Certificate -CertStoreLocation "cert:\LocalMachine\My" -Template "Machine"
--- readme.md
+++ readme.md
@@ -30,4 +30,10 @@ New-SelfSignedCertificate -DnsName "ADCS-Client" -CertStoreLocation $certStore -
 ### Check Powershell version
 ```Powershell
 $PSVersionTable.PSVersion
+
+### How to retrieve the certificate from the certificate store
+```Powershell
+$certStore = "cert:\LocalMachine\My"
+$exportDir = "C:\Users\Administrator\Desktop\work"
+Get-ChildItem -Path $certStore | ForEach-Object { Export-Certificate -Cert $_ -FilePath "$exportDir$($_.Thumbprint).cer" }
 ```

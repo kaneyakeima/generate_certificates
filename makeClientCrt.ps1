4a1a404cf922a94d996007a864039d851687f27a
kanepon <cindyrotus@gmail.com>
2023-05-15 16:31:50 +0900

    20230515-163150

--- makeClientCrt.ps1
+++ makeClientCrt.ps1
@@ -1,12 +1,8 @@
-# Reads SAN and email address from external text file and issues certificate
 $userList = Import-Csv -Path ".\user-list.csv"
 foreach ($user in $userList) {
-  $certProps = @{
-    SubjectName = "CN=$user.SAN"
-    CertStoreLocation = "cert:\LocalMachine\My"
-    Template = "Machine"
-    DnsName = $user.DNS
-  }
-  Get-Certificate @certProps
+  $cn = $user.CN
+  $san = $user.SAN
+  $CertDir = "cert:\LocalMachine\My"
+  $Template = "computer2"
+  Get-Certificate -Template $Template -Subject CN=$cn -CertStoreLocation $CertDir -DnsName $san
 }
-#Get-Certificate -CertStoreLocation "cert:\LocalMachine\My" -Template "Machine"

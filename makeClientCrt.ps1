05a6e12bf060fe443bc3b24cce6c2f9a4a99a75a
kaneyakeima <86823299+kaneyakeima@users.noreply.github.com>
2023-05-23 15:24:57 +0900

    20230523-1524

--- makeClientCrt.ps1
+++ makeClientCrt.ps1
@@ -1,8 +1,44 @@
-$userList = Import-Csv -Path ".\user-list.csv"
-foreach ($user in $userList) {
-  $cn = $user.CN
-  $san = $user.SAN
-  $CertDir = "cert:\LocalMachine\My"
-  $Template = "[Template Name]"
-  Get-Certificate -Template $Template -Subject CN=$cn -CertStoreLocation $CertDir -DnsName $san
+$pw = "password"
+$ca = ".\ppp-HOGE-CA"
+$ul = Import-Csv -Path "user-list.csv"
+$dt = Get-Date -Format "yyyyMMdd_HHmmss"
+$log = "$dt" + ".log"
+New-Item "cer" -ItemType Directory -Force
+New-Item "inf" -ItemType Directory -Force
+New-Item "csr" -ItemType Directory -Force
+Write-Output "###### SINGLE" >> $log
+function main {
+  foreach ($user in $ul){
+    $cn = $user.cn
+    Write-Output "###### START [ $cn ]  "
+    $email = $user.email
+    $upn = $user.upn
+    $infval = @"
+  [NewRequest]
+  Subject = "CN=$cn"
+  Exportable = TRUE
+  ExportableEncrypted = TRUE
+  [RequestAttributes]
+  CertificateTemplate = WorkspaceONE
+  [Extensions]
+  2.5.29.17 = "{text}"
+  _continue_ = "email=$email&"
+  _continue_ = "UPN=$upn"
+"@
+    $inf = $user.cn + ".inf"
+    $csr = $user.cn + ".csr"
+    $cer = $user.cn + ".cer"
+    Write-Output "### INF-FILE"
+    New-Item -Force .\inf\$inf -type File -value $infval
+    Write-Output "### CSR"
+    certreq -new -f .\inf\$inf .\csr\$csr
+    Write-Output "### CA-SIGN"
+    certreq -submit -f -config $ca .\csr\$csr .\cer\$cer
+    Write-Output "### INSTALL"
+    certreq -accept .\cer\$cn.cer
+    Write-Output "### EXPORT"
+    certutil -f -exportpfx -p $pw My $cn "$cn.pfx" ExtendedProperties,ExportParameters
+  }
 }
+$sec = (Measure-Command{ main >> $log}).TotalSeconds
+Write-Output "###### Total processing time","$sec sec." >> $log

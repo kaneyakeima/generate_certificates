f6549cc6aaeebc8a8bfc4c8060a8cd923bba837d
kanepon <cindyrotus@gmail.com>
2023-05-27 08:09:24 +0900

    20230526-0809

--- createUserList.ps1
+++ createUserList.ps1
@@ -1,9 +1,9 @@
 $array = "cn,upn,email`r`n"
 for($i=1;$i -lt 6;$i++){
-$array += "test$i,test${i}upn,test$i@ppp.local"
+$array += "CommonName$i,UserPrincipalName${i}upn,Emai$i@example.com,UserFamilyName${i},UserLastName${i}"
 $array += "`r`n"
 }
-$fileName = "user-list.csv"
+$fileName = "user_list.csv"
 New-Item -f $fileName -type file -value $array
 Write-Host "`r`n###---VVV------<$fileName>------VVV---###"
 type $fileName
